737e1a39473c9ed5758d52635094b660d853f2b4
Michael Gracy <michael.gracy@cadreon.com>
2016-09-14 01:17:49 -0700

    Intial commit

---
+++ aws-pricer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+URL="http://a0.awsstatic.com/pricing/1/ec2/ri-v2/linux-unix-shared.min.js"
+
+(echo 'function callback(data) { console.log(JSON.stringify(data)); }'; curl -s "$URL") |\
+  node |\
+  jq -r '.config.regions[] |
+      select(.region == "us-east-1") |
+      .instanceTypes[] |
+      [
+        .type,
+        ( .terms[] | select(.term == "yrTerm1") | .onDemandHourly[].prices.USD ),
+        (
+          .terms[] | select(.term == "yrTerm1") | .purchaseOptions |
+          map(.valueColumns[] | select(.name == "effectiveHourly") | .prices.USD)[]
+        ),
+        (
+          .terms[] | select(.term == "yrTerm3") | .purchaseOptions |
+          map(.valueColumns[] | select(.name == "effectiveHourly") | .prices.USD)[]
+        )
+      ] |
+      join("\t")
+      '
